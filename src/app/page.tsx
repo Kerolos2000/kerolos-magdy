e26341f5a50fc34bda7008ec1afaa328d17ae58b
Kerolos2000 <kero.magdy2000@gmail.com>
2025-10-10 03:41:58 +0300

    feat: 🎸 add base of HomeTimelineSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,16 @@ import {
   HomeAboutMeSection,
   HomeBreakSection,
   HomeHeroSection,
+  HomeTimelineSection,
 } from "src/components";
 
 export default function Home() {
   return (
-    <main id="home">
+    <main id="home-page">
       <HomeHeroSection />
       <HomeAboutMeSection />
       <HomeBreakSection />
+      <HomeTimelineSection />
     </main>
   );
 }
--- src/components/sections/home/index.ts
+++ src/components/sections/home/index.ts
@@ -1,3 +1,4 @@
 export { default as HomeAboutMeSection } from "./about-me-section";
 export { default as HomeBreakSection } from "./break-section";
 export { default as HomeHeroSection } from "./hero-section";
+export { default as HomeTimelineSection } from "./timeline-section";
