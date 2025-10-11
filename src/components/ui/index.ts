99fafb8b09355aa5e2de25b82968b3e319c5380e
Kerolos2000 <kero.magdy2000@gmail.com>
2025-10-12 01:24:56 +0300

    feat: 🎸 add NumberTicker component

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -3,6 +3,7 @@ export * from "./animated-gradient-text";
 export * from "./background-ripple-effect";
 export * from "./floating-dock";
 export * from "./highlighter";
+export * from "./number-ticker";
 export * from "./scroll-based-velocity";
 export * from "./text-hover-effect";
 export * from "./timeline";
