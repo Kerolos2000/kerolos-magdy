c322be2fcd4f507df232517c2996015ab6cd3a01
Kerolos2000 <kero.magdy2000@gmail.com>
2025-10-29 03:00:55 +0300

    feat: 🎸 refactor Home component to use dynamic imports for sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,33 @@
-import {
-  HomeAboutMeSection,
-  HomeBreakSection,
-  HomeCounterSection,
-  HomeExperienceSection,
-  HomeGetInTouchSection,
-  HomeHeroSection,
-  HomeProjectsSection,
-} from "src/components";
+import dynamic from "next/dynamic";
+
+const HomeHeroSection = dynamic(
+  () => import("src/components/sections/home/hero-section"),
+  { ssr: true }
+);
+const HomeAboutMeSection = dynamic(
+  () => import("src/components/sections/home/about-me-section"),
+  { ssr: true }
+);
+const HomeBreakSection = dynamic(
+  () => import("src/components/sections/home/break-section"),
+  { ssr: true }
+);
+const HomeExperienceSection = dynamic(
+  () => import("src/components/sections/home/experience-section"),
+  { ssr: true }
+);
+const HomeCounterSection = dynamic(
+  () => import("src/components/sections/home/counter-section"),
+  { ssr: true }
+);
+const HomeProjectsSection = dynamic(
+  () => import("src/components/sections/home/projects-section"),
+  { ssr: true }
+);
+const HomeGetInTouchSection = dynamic(
+  () => import("src/components/sections/home/get-in-touch-section"),
+  { ssr: true }
+);
 
 export default function Home() {
   return (
