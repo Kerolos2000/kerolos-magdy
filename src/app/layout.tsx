aa98ad832804c81cea4195e240118b7d4fe375d3
Kerolos2000 <kero.magdy2000@gmail.com>
2025-11-11 03:46:15 +0200

    feat: 🎸 add Analytics and SpeedInsights from vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from 'next';
 import { ThemeProvider } from 'next-themes';
 import { Inter } from 'next/font/google';
@@ -38,6 +40,8 @@ export default function RootLayout({
 					<Footer />
 				</ThemeProvider>
 				<SmoothScroll />
+				<Analytics mode='production' />
+				<SpeedInsights />
 			</body>
 		</html>
 	);
